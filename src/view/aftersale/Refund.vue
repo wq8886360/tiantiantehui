db534a7a93384f47ca4a577f3eaa26d38648ea97
wuzuojie <15727841808@163.com>
2017-12-27 15:25:57 +0800

    change

---
+++ src/view/aftersale/Refund.vue
@@ -0,0 +1,33 @@
+<template>
+	<div>
+		<exchangeTop></exchangeTop>
+		<offcialshop></offcialshop>
+		<relationmer></relationmer>
+	</div>
+</template>
+<script>
+import exchangeTop from './public/exchangeTop.vue'
+import offcialshop from './public/offcialshop.vue'
+import relationmer from './public/relationmer.vue'
+export default{
+	components: {
+		'exchangeTop':exchangeTop,
+		'offcialshop':offcialshop,
+		'relationmer':relationmer,
+	},
+	data(){
+		return{
+
+		}
+	},
+	methods:{
+
+	},
+	created(){
+
+	}
+}	
+</script>
+<style>
+
+</style>
