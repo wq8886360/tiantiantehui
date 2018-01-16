da1c9ac12071e57c83f8196ecccd2b4c2392418d
葛佳阳 <18358205737@163.com>
2018-01-16 14:34:41 +0800

    Api

--- src/http/api.js
+++ src/http/api.js
@@ -90,3 +90,5 @@ export const refundapplyTerrace = params => post('/member/refund/callplat',param
 export const refundapplyLogList = params =>get('/member/refund/applyLogList',params);//协商历史
 export const refundgetShipInfo = params =>  get('/member/refund/getShipInfo',params);//获取提交的物流信息
 export const refundsaveShipInfo = params => post('/member/refund/saveShipInfo',params);//提交物流信息
+
+export const toplist = params =>get('/goods/top',params);//排行榜
